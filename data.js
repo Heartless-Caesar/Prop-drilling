d6485dddd6de836370b7258d1b4494588d1812b1
Heartless_Caesar <82420106+Heartless-Caesar@users.noreply.github.com>
2021-08-04 21:45:03 -0300

    Add files via upload
    Basic prop drilling example, passing through a function and properties through nested components

---
+++ data.js
@@ -0,0 +1,17 @@
+export const data = [
+ {
+  id:1,
+  name :"Marcus"},
+ {
+     id:2,
+     name: "Caesar"
+},
+ {
+     id:3,
+     name:"Hugh"
+},
+ {
+     id:4,
+     name:"Nasir"
+},
+];
--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-const sum = () => {
-  return 10 + 10
+//USE CONTEXT
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {useState} from 'react'
+import {data} from './data'
+import './index.css'
+
+const UseContext = () => {
+const [people, setPeople] = useState(data)
+
+const removePerson = (id) => {
+    setPeople((person)=>{
+        return(
+            person.filter((e)=> e.id !== id)
+        )
+    })
+ }
+
+    return(
+        <>
+         <List remove={removePerson} people={people}/>
+        </>
+    )
 }
+
+const List = ({people, remove}) => {
+    return(
+        <>
+        {people.map((person) => {
+            return(
+                <div>
+                 <Individual removePer={remove} key={person.id} {...person}/>
+                </div>)
+            })
+           }
+      </>)
+}
+
+const Individual = ({id, name, removePer}) => {
+  return(
+      <div>
+       <h1>{name}</h1>
+       <button type="button" onClick={()=> removePer(id)}>Remove</button>
+      </div>)
+}
+
+ReactDOM.render(<UseContext />, document.getElementById('root'))
